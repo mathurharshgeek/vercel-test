de938525a6c9aea07d83cbb5d4b74dd3891f2717
mathurharshgeek <harshonlaptop@gmail.com>
2023-07-18 18:48:00 +0530

    tryingggg

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express=require('express');
 const app= express();
 const routes=require('./routes')(app)
 
-app.listen(5000,()=>{console.log("Server started")})
+app.listen(8081,()=>{console.log("Server started")})
